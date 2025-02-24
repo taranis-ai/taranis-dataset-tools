79966f98bd265c737e4675c872d1fabb4fd2415e
llinauer <l.linauer@gmx.at>
2025-02-24 12:51:00 +0100

    Add more fixtures to conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,16 +5,24 @@ import pandas as pd
 from dotenv import load_dotenv
 
 
-base_dir = os.path.dirname(os.path.abspath(__file__))
-env_file = os.path.join(base_dir, ".env.test")
+test_dir = os.path.dirname(os.path.abspath(__file__))
+env_file = os.path.join(test_dir, ".env.test")
 current_path = os.getcwd()
 
 load_dotenv(dotenv_path=env_file, override=True)
 
 @pytest.fixture(scope="session")
-def taranis_dataset_json():
-    yield json.loads("assets/taranis_story_export_feb_2025_tiny.json")
+def taranis_dataset_path():
+    yield os.path.abspath(os.path.join(test_dir, f"{os.getenv('TARANIS_DATASET_PATH')}"))
+
+@pytest.fixture(scope="session")
+def tokenizer():
+    yield os.getenv("PREPROCESS_TOKENIZER")
+
+@pytest.fixture(scope="session")
+def taranis_dataset_json(taranis_dataset_path):
+    yield json.loads(taranis_dataset_path)
 
 @pytest.fixture(scope="session")
 def taranis_dataset_df():
-    yield pd.read_json("assets/taranis_story_export_feb_2025_tiny.json")
+    yield pd.read_json(taranis_dataset_path)
