a339656faaac34207685c8b991c405de66ba4def
llinauer <l.linauer@gmx.at>
2025-03-20 09:11:17 +0100

    Add default preprocess tokenizer; remove TEACHER from configs; make only DB_PATH and TABLE_NAME required configs

--- taranis_ds/config.py
+++ taranis_ds/config.py
@@ -9,24 +9,22 @@ class Settings(BaseSettings):
 
     TARANIS_DATASET_PATH: str = ""
 
-    PREPROCESS_TOKENIZER: str = ""
+    PREPROCESS_TOKENIZER: str = "facebook/bart-large-cnn"
     PREPROCESS_MAX_TOKENS: int
 
-    SUMMARY_TEACHER_MODEL: str = "Mistral-Nemo-Instruct-2407"
-    SUMMARY_TEACHER_ENDPOINT: str = "https://mistral-nemo-instruct-2407.endpoints.kepler.ai.cloud.ovh.net/api/openai_compat/v1"
+    SUMMARY_MODEL: str = "Mistral-Nemo-Instruct-2407"
+    SUMMARY_ENDPOINT: str = "https://mistral-nemo-instruct-2407.endpoints.kepler.ai.cloud.ovh.net/api/openai_compat/v1"
     SUMMARY_MAX_TOKENS: int = 130
-    SUMMARY_TEACHER_API_KEY: str = ""
+    SUMMARY_API_KEY: str = ""
     SUMMARY_MAX_LENGTH: int = 50
-    SUMMARY_TEACHER_TEMPERATURE: float = 0.7
+    SUMMARY_TEMPERATURE: float = 0.7
     SUMMARY_QUALITY_THRESHOLD: float = 0.6
     SUMMARY_REQUEST_WAIT_TIME: float = 2.0
 
-    CYBERSEC_CLASS_TEACHER_MODEL: str = "Mixtral-8x7B-Instruct-v0.1"
-    CYBERSEC_CLASS_TEACHER_ENDPOINT: str = (
-        "https://mixtral-8x7b-instruct-v01.endpoints.kepler.ai.cloud.ovh.net/api/openai_compat/v1/chat/completions"
-    )
-    CYBERSEC_CLASS_TEACHER_API_KEY: str = ""
-    CYBERSEC_CLASS_TEACHER_TEMPERATURE: float = 0.7
+    CYBERSEC_CLASS_MODEL: str = "Mixtral-8x7B-Instruct-v0.1"
+    CYBERSEC_CLASS_ENDPOINT: str = "https://mixtral-8x7b-instruct-v01.endpoints.kepler.ai.cloud.ovh.net/api/openai_compat/v1/chat/completions"
+    CYBERSEC_CLASS_API_KEY: str = ""
+    CYBERSEC_CLASS_TEMPERATURE: float = 0.7
     CYBERSEC_CLASS_REQUEST_WAIT_TIME: float = 2.0
 
     DEBUG: bool = False
@@ -34,7 +32,7 @@ class Settings(BaseSettings):
     DB_PATH: str = "taranis_data_pipeline.db"
     TABLE_NAME: str = "results"
 
-    @field_validator("TARANIS_DATASET_PATH", "PREPROCESS_TOKENIZER", "SUMMARY_TEACHER_API_KEY", "DB_PATH", "TABLE_NAME", mode="before")
+    @field_validator("DB_PATH", "TABLE_NAME", mode="before")
     def check_non_empty_string(cls, v: str, info: ValidationInfo) -> str:
         if not isinstance(v, str) or not v.strip():
             raise ValueError(f"{info.field_name} must be a non-empty string")
