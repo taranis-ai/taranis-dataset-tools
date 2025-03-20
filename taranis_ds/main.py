688755940ef406da49cc72eeb0e274ba842c6066
llinauer <l.linauer@gmx.at>
2025-03-20 14:41:58 +0100

    Add main.py -> contains entrypoints

---
+++ taranis_ds/main.py
@@ -0,0 +1,47 @@
+"""
+main.py
+
+Run the pipeline
+"""
+
+from pathlib import Path
+
+import pandas as pd
+
+from taranis_ds.config import Config
+from taranis_ds.log import get_logger
+from taranis_ds.misc import check_config, save_df_to_table
+from taranis_ds.persist import get_db_connection
+
+
+logger = get_logger(__name__)
+
+
+def save_to_db():
+    if not check_config("PROCESSED_DATASET_PATH", str):
+        return
+
+    if not Path(Config.PROCESSED_DATASET_PATH).exists():
+        logger.error("%s does not exist", Config.PROCESSED_DATASET_PATH)
+        return
+
+    if not Config.PROCESSED_DATASET_PATH.endswith(".json"):
+        logger.error("%s must be in .json format", Config.PROCESSED_DATASET_PATH)
+        return
+
+    try:
+        df = pd.read_json(Config.PROCESSED_DATASET_PATH)
+    except ValueError as e:
+        logger.error("Could not load %s. Error: %s", Config.PROCESSED_DATASET_PATH, e)
+        return
+
+    connection = get_db_connection(Config.DB_PATH)
+    save_df_to_table(df, Config.TABLE_NAME, connection)
+
+
+def run():
+    pass
+
+
+if __name__ == "__main__":
+    run()
