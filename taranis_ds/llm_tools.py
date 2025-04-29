c6aa16da594052dbb479cf7e1188f276c64e7e81
llinauer <l.linauer@gmx.at>
2025-04-29 10:47:05 +0200

    Add catch for general OutputParserException to prompt_model_with_retry function

--- taranis_ds/llm_tools.py
+++ taranis_ds/llm_tools.py
@@ -8,10 +8,16 @@ import time
 
 import httpx
 from langchain.prompts import PromptTemplate
+from langchain_core.exceptions import OutputParserException
 from langchain_core.language_models.chat_models import BaseChatModel
 from langchain_core.output_parsers import BaseOutputParser
 from langchain_core.runnables import RunnableLambda, RunnableParallel
 from langchain_core.runnables.base import RunnableSequence
+
+from taranis_ds.log import get_logger
+
+
+logger = get_logger(__name__)
 
 
 def create_chain(model: BaseChatModel, prompt: PromptTemplate, parser: BaseOutputParser):
@@ -20,7 +26,7 @@ def create_chain(model: BaseChatModel, prompt: PromptTemplate, parser: BaseOutpu
     return chain
 
 
-def prompt_model_with_retry(chain: RunnableSequence, model_inputs: dict, max_retries: int = 3):
+def prompt_model_with_retry(chain: RunnableSequence, model_inputs: dict, max_retries: int = 3) -> tuple[str, str]:
     for _ in range(max_retries):
         try:
             output, status = "", "OK"
@@ -33,6 +39,10 @@ def prompt_model_with_retry(chain: RunnableSequence, model_inputs: dict, max_ret
                 time.sleep(0.5)
             else:
                 break
+        except OutputParserException as e:
+            status = "ERROR"
+            logger.error("Could not parse LLM output. Error: %s Skipping.", e)
+
     else:  # got 429 on all tries
         status = "TOO_MANY_REQUESTS"
 
